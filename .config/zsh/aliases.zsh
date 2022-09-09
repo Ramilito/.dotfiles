7acb516b5f50a5cef81b4f6a7599ca1956c5f138
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-09-10 00:02:01 +0200

    removed history, added alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,5 +1,5 @@
 alias homeserver="sudo sshfs -o allow_other,default_permissions,nonempty ubuntu@ubuntu.lan:/home/ubuntu/ /mnt/home-server"
-# alias config='lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+alias config_lazy='lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 alias k="kubectl"
