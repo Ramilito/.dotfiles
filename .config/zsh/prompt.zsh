c7cae68406e5a40eb5914e08132e9c305f16130d
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-09-22 06:06:26 +0200

    reset leftprompt

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -20,6 +20,7 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%} %{$fg[yellow]%}✗"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}"
 
-PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+# PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+PROMPT=""
 PROMPT+=' %{$fg[magenta]%}%c%{$reset_color%} $(git_prompt_info)'
 
