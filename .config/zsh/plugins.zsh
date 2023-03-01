62a0408a46d1758ef9b92f96a09dedc4a5748c77
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-03-01 12:02:59 +0100

    fix lazyload nvm

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,7 +1,7 @@
 export NVM_LAZY_LOAD=true
 export NVM_NO_USE=true
 export NVM_COMPLETION=true
-export NVM_LAZY_LOAD_EXTRA_COMMANDS=('lvim')
+export NVM_LAZY_LOAD_EXTRA_COMMANDS=('nvim')
 
 plugins=(git vi-mode fzf-tab)
 plugins+=(zsh-nvm)
