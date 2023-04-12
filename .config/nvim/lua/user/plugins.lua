fd0a533b00d6cf627bc5c1ce6a234c5034ffb9ca
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-04-12 10:10:43 +0200

    fix theme

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,5 +1,23 @@
 local M = {
-  -- {"folke/tokyonight.nvim", lazy = false, priority = 100 },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     -- load the colorscheme here
+  --     vim.cmd([[colorscheme tokyonight]])
+  --   end,
+  -- },
+  {
+    "catppuccin/nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  },
+
   -- { "williamboman/mason-lspconfig.nvim", lazy = false },
   "nvim-lua/plenary.nvim",
   -- "arkav/lualine-lsp-progress",
