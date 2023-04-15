0875fdfc450575246618a33414471c745138d320
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-04-15 02:27:39 +0200

    using stable branch

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,13 +4,12 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
-    "--single-branch",
     "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
-
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require("user.options")
 require("user.keymappings")
