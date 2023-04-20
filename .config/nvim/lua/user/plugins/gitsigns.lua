2e586319c33071606ad6364f2f57025a0f56528d
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-04-20 10:58:19 +0200

    better gitsigns

--- .config/nvim/lua/user/plugins/gitsigns.lua
+++ .config/nvim/lua/user/plugins/gitsigns.lua
@@ -1,21 +1,23 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = "BufReadPre",
-  opts = {
-    signs = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "契" },
-      topdelete = { text = "契" },
-      changedelete = { text = "▎" },
-      untracked = { text = "▎" },
-    },
-    on_attach = function(buffer)
-      local gs = package.loaded.gitsigns
+	"lewis6991/gitsigns.nvim",
+	event = "BufReadPre",
+	opts = {
+		numhl = true,
+		signcolumn = false,
+		signs = {
+			add = { text = "▎" },
+			change = { text = "▎" },
+			delete = { text = "契" },
+			topdelete = { text = "契" },
+			changedelete = { text = "▎" },
+			untracked = { text = "▎" },
+		},
+		on_attach = function(buffer)
+			local gs = package.loaded.gitsigns
 
-      local function map(mode, l, r, desc)
-        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-      end
+			local function map(mode, l, r, desc)
+				vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+			end
 
       -- stylua: ignore start
       map("n", "]h", gs.next_hunk, "Next Hunk")
@@ -30,6 +32,6 @@ return {
       map("n", "<leader>ghd", gs.diffthis, "Diff This")
       map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-    end,
-  },
+		end,
+	},
 }
