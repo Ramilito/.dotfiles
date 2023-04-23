b8def61840b6a866cc2b803eb2ef9d9c6e85d390
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-04-23 19:43:41 +0200

    using diff theme

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -8,13 +8,24 @@ local M = {
   --     vim.cmd([[colorscheme tokyonight]])
   --   end,
   -- },
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     -- load the colorscheme here
+  --     vim.cmd([[colorscheme catppuccin]])
+  --   end,
+  -- },
+
   {
-    "catppuccin/nvim",
+    "Mofiqul/vscode.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme catppuccin]])
+      vim.cmd([[colorscheme vscode]])
+      vim.o.background = 'dark'
     end,
   },
 
