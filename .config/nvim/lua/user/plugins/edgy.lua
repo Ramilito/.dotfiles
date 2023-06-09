8b4512efcaaf715c66afe5f8e3d45224facb5136
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-06-09 20:41:51 +0200

    removed commented code

--- .config/nvim/lua/user/plugins/edgy.lua
+++ .config/nvim/lua/user/plugins/edgy.lua
@@ -1,10 +1,3 @@
--- return {
--- 	"folke/edgy.nvim",
--- 	event = "VeryLazy",
--- 	opts = {},
--- 	dependencies = { "echasnovski/mini.animate" },
--- }
-
 return {
 	-- edgy
 	{
@@ -83,36 +76,4 @@ return {
 			},
 		},
 	},
-
-	-- Fix bufferline offsets when edgy is loaded
-	-- {
-	--   "akinsho/bufferline.nvim",
-	--   optional = true,
-	--   opts = function()
-	--     local Offset = require("bufferline.offset")
-	--     if not Offset.edgy then
-	--       local get = Offset.get
-	--       Offset.get = function()
-	--         if package.loaded.edgy then
-	--           local layout = require("edgy.config").layout
-	--           local ret = { left = "", left_size = 0, right = "", right_size = 0 }
-	--           for _, pos in ipairs({ "left", "right" }) do
-	--             local sb = layout[pos]
-	--             if sb and #sb.wins > 0 then
-	--               local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
-	--               ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
-	--               ret[pos .. "_size"] = sb.bounds.width
-	--             end
-	--           end
-	--           ret.total_size = ret.left_size + ret.right_size
-	--           if ret.total_size > 0 then
-	--             return ret
-	--           end
-	--         end
-	--         return get()
-	--       end
-	--       Offset.edgy = true
-	--     end
-	--   end,
-	-- },
 }
