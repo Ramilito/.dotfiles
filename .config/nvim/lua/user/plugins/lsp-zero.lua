1fc4d6640cdb64ce627d5d3d4ee17acbd67e4389
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-07-12 02:25:36 +0200

    indent correctly

--- .config/nvim/lua/user/plugins/lsp-zero.lua
+++ .config/nvim/lua/user/plugins/lsp-zero.lua
@@ -1,67 +1,68 @@
 local M = {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  event = "VeryLazy",
-  dependencies = {
-    -- LSP Support
-    { 'neovim/nvim-lspconfig' }, -- Required
-    { 'williamboman/mason.nvim' }, -- Optional
-    { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v2.x",
+	event = "VeryLazy",
+	dependencies = {
+		-- LSP Support
+		{
+			"neovim/nvim-lspconfig",
+		}, -- Required
+		{ "williamboman/mason.nvim" }, -- Optional
+		{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp' }, -- Required
-    { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-    { 'hrsh7th/cmp-buffer' }, -- Optional
-    { 'hrsh7th/cmp-path' }, -- Optional
-    { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-    { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+		-- Autocompletion
+		{ "hrsh7th/nvim-cmp" }, -- Required
+		{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+		{ "hrsh7th/cmp-buffer" }, -- Optional
+		{ "hrsh7th/cmp-path" }, -- Optional
+		{ "saadparwaiz1/cmp_luasnip" }, -- Optional
+		{ "hrsh7th/cmp-nvim-lua" }, -- Optional
 
-    -- Snippets
-    { 'L3MON4D3/LuaSnip' }, -- Required
-    { 'rafamadriz/friendly-snippets' }, -- Optional
-  },
-  config = function()
-    local lsp = require('lsp-zero')
-    local cmp = require('cmp')
+		-- Snippets
+		{ "L3MON4D3/LuaSnip" }, -- Required
+		{ "rafamadriz/friendly-snippets" }, -- Optional
+	},
+	config = function()
+		local lsp = require("lsp-zero")
+		local cmp = require("cmp")
 
-    lsp.preset('recommended')
+		lsp.preset("recommended")
 
-    local cmp_mappings = lsp.defaults.cmp_mappings({
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({behavior = cmp.ConfirmBehavior.Replace, select = true }),
-    })
+		local cmp_mappings = lsp.defaults.cmp_mappings({
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<C-e>"] = cmp.mapping.abort(),
+			["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+		})
 
-    local cmp_sources = lsp.defaults.cmp_sources({ name = "copilot" })
-    table.insert(cmp_sources, { name = "copilot" })
+		local cmp_sources = lsp.defaults.cmp_sources({ name = "copilot" })
+		table.insert(cmp_sources, { name = "copilot" })
 
-    lsp.setup_nvim_cmp({
-      mapping = cmp_mappings
-    })
+		lsp.setup_nvim_cmp({
+			mapping = cmp_mappings,
+		})
 
-    lsp.on_attach(function(client, bufnr)
-      require('nvim-navic').attach(client, bufnr)
-    end)
+		lsp.on_attach(function(client, bufnr)
+			require("nvim-navic").attach(client, bufnr)
+		end)
 
-    lsp.setup()
+		lsp.setup()
 
-    local cmp_config = lsp.defaults.cmp_config({
-      sources = cmp_sources,
-    })
+		local cmp_config = lsp.defaults.cmp_config({
+			sources = cmp_sources,
+		})
 
-    cmp.setup(cmp_config)
+		cmp.setup(cmp_config)
 
-    local null_ls = require('null-ls')
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        -- Replace these with the tools you have installed
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.stylua,
-      }
-    })
-
-  end
+		null_ls.setup({
+			sources = {
+				-- Replace these with the tools you have installed
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.eslint,
+				null_ls.builtins.formatting.stylua,
+			},
+		})
+	end,
 }
 return M
