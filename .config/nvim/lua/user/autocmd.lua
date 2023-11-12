d244025e0fe4a719b54467e15c01303904edc7fc
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-11-12 23:15:18 +0100

    don't auto open preview

--- .config/nvim/lua/user/autocmd.lua
+++ .config/nvim/lua/user/autocmd.lua
@@ -67,12 +67,12 @@ vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "InsertLeave" }, {
   end,
 })
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "OilEnter",
-  callback = vim.schedule_wrap(function(args)
-    local oil = require("oil")
-    if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
-      oil.select({ preview = true })
-    end
-  end),
-})
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = "OilEnter",
+--   callback = vim.schedule_wrap(function(args)
+--     local oil = require("oil")
+--     if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+--       oil.select({ preview = true })
+--     end
+--   end),
+-- })
