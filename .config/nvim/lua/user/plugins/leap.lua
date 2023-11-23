1bb7e68ac5802adcb0db2c6030602fa4319d20a9
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-11-23 04:31:53 +0100

    no need to comment out plugins anymore

--- .config/nvim/lua/user/plugins/leap.lua
+++ .config/nvim/lua/user/plugins/leap.lua
@@ -1,12 +1,10 @@
--- return {
---   {
---     "ggandor/leap.nvim",
---     event = "VeryLazy",
---     dependencies = { { "ggandor/flit.nvim", opts = { labeled_modes = "nv" } } },
---     config = function()
---       require("leap").add_default_mappings(false)
---     end,
---   },
--- } 
---
-return {}
+return {
+  {
+    "ggandor/leap.nvim",
+    event = "VeryLazy",
+    dependencies = { { "ggandor/flit.nvim", opts = { labeled_modes = "nv" } } },
+    config = function()
+      require("leap").add_default_mappings(false)
+    end,
+  },
+}
