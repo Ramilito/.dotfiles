d6327cc6bc9e3ca15a76fc43d8e0c78cd5a7585c
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-12-14 05:00:04 +0100

    lazy comment

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -90,7 +90,15 @@ local M = {
   },
   {
     "numToStr/Comment.nvim",
-    event = "VeryLazy",
+    lazy = true,
+    keys = {
+      { "gcc", mode = "n",          desc = "Comment toggle current line" },
+      { "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
+      { "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
+      { "gbc", mode = "n",          desc = "Comment toggle current block" },
+      { "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+      { "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
+    },
     config = function()
       require("Comment").setup({
         pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
