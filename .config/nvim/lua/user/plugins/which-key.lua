3cd77fbca25891ae4b1f35fc6a892caec95a2352
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-12-14 05:00:36 +0100

    lazy whichkey

--- .config/nvim/lua/user/plugins/which-key.lua
+++ .config/nvim/lua/user/plugins/which-key.lua
@@ -1,6 +1,6 @@
 return { "folke/which-key.nvim",
-  -- keys = { "<leader>", "g" },
-  event = "VeryLazy",
+  keys = { "<leader>", "g" },
+  lazy = true,
 
   config = function()
     local wk = require("which-key")
@@ -19,29 +19,5 @@ return { "folke/which-key.nvim",
       ["<leader>w"] = { name = "+windows" },
       ["<leader>x"] = { name = "+trouble" },
     })
-    -- require("which-key").setup({
-    --   plugins = {
-    --     marks = true,
-    --     registers = true,
-    --     spelling = {
-    --       enabled = true,
-    --       suggestions = 20,
-    --     },
-    --   },
-    --
-    --   window = {
-    --     border = "single", -- none, single, double, shadow
-    --     position = "bottom", -- bottom, top
-    --     margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    --     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-    --     winblend = 0,
-    --   },
-    --   layout = {
-    --     height = { min = 4, max = 25 }, -- min and max height of the columns
-    --     width = { min = 20, max = 50 }, -- min and max width of the columns
-    --     spacing = 3, -- spacing between columns
-    --     align = "left", -- align columns left, center or right
-    --   },
-    -- })
   end,
 }
