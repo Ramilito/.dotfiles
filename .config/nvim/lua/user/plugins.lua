4d25b296ca2b505c279915da0b4cfcccaa3a3ef8
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-03-10 21:37:36 +0100

    add eyeliner

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -100,6 +100,15 @@ local M = {
     end,
 
   },
+  {
+    'jinh0/eyeliner.nvim',
+    config = function()
+    require("eyeliner").setup({
+      highlight_on_key = true, -- show highlights only after keypress
+      dim = false, -- dim all other characters if set to true (recommended!)
+    })
+    end
+  }
 }
 
 return M
