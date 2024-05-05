54dda79cbaf8310d355fea8cc54667366d3ec642
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-05-05 23:28:34 +0200

    add statuscolumn autocmd

--- .config/nvim/lua/user/autocmd.lua
+++ .config/nvim/lua/user/autocmd.lua
@@ -20,6 +20,40 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
+vim.api.nvim_create_autocmd({ "BufEnter", "VimEnter" }, {
+	group = augroup("statuscolumn"),
+	callback = function()
+		local winbar_filetype_exclude = {
+			"help",
+			"startify",
+			"dashboard",
+			"packer",
+			"neo-tree",
+			"neogitstatus",
+			"NvimTree",
+			"Trouble",
+			"alpha",
+			"lir",
+			"Outline",
+			"spectre_panel",
+			"toggleterm",
+			"TelescopePrompt",
+			"prompt",
+		}
+
+		if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
+			return
+		end
+
+		local win_number = vim.api.nvim_get_current_win()
+		local config = vim.api.nvim_win_get_config(win_number)
+
+		if config.relative == "" then
+			vim.opt_local.statuscolumn = require("user.statuscolumn")
+		end
+	end,
+})
+
 -- vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "InsertLeave" }, {
 -- 	pattern = "*.norg",
 -- 	group = augroup("auto-save"),
