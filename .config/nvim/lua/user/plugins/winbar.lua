9d06bbe03feaa2f02543b8902659c5abc8204a2e
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-05-05 23:28:26 +0200

    add dep and correct event to load

--- .config/nvim/lua/user/plugins/winbar.lua
+++ .config/nvim/lua/user/plugins/winbar.lua
@@ -1,9 +1,10 @@
 return {
 	{
 		"ramilito/winbar.nvim",
-		lazy = false,
+		event = "VimEnter",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
+			"lewis6991/gitsigns.nvim",
 		},
 		config = function()
 			local wb = require("winbar")
