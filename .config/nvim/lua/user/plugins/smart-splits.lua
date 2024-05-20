b9a2e9fe3f9a2c7a13573dc55e529b8a8c1790ed
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-05-20 15:07:25 +0200

    dont wrap on edge

--- .config/nvim/lua/user/plugins/smart-splits.lua
+++ .config/nvim/lua/user/plugins/smart-splits.lua
@@ -3,7 +3,7 @@ return {
 		"mrjones2014/smart-splits.nvim",
 		lazy = false,
 		config = function()
-			require("smart-splits").setup({})
+			require("smart-splits").setup({ at_edge = nil })
 		end,
 	},
 }
