9b11b7e6d39f0f9da7529631070b18e97aad1bd8
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-07-03 10:00:44 +0200

    add sylua

--- .config/nvim/lua/user/plugins/kubectl.lua
+++ .config/nvim/lua/user/plugins/kubectl.lua
@@ -1,34 +1,34 @@
 return {
-	{
-		"ramilito/kubectl.nvim",
-		dir = "~/workspace/mine/kubectl.nvim/",
-		keys = {
-			{
-				"<leader>k",
-				function()
-					require("kubectl").open()
-				end,
-				desc = "Kubectl",
-			},
-		},
-		config = function()
-			require("kubectl").setup({
-				namespace = "All",
-				notifications = {
-					enabled = true,
-					verbose = false,
-				},
-				auto_refresh = {
-					enabled = true,
-					interval = 2000, -- milliseconds
-				},
-				hints = true,
-				context = true,
-				float_size = {
-					width = 0.9,
-					height = 0.8,
-				},
-			})
-		end,
-	},
+  {
+    "ramilito/kubectl.nvim",
+    dir = "~/workspace/mine/kubectl.nvim/",
+    keys = {
+      {
+        "<leader>k",
+        function()
+          require("kubectl").open()
+        end,
+        desc = "Kubectl",
+      },
+    },
+    config = function()
+      require("kubectl").setup({
+        namespace = "All",
+        notifications = {
+          enabled = true,
+          verbose = false,
+        },
+        auto_refresh = {
+          enabled = true,
+          interval = 2000, -- milliseconds
+        },
+        hints = true,
+        context = true,
+        float_size = {
+          width = 0.9,
+          height = 0.8,
+        },
+      })
+    end,
+  },
 }
