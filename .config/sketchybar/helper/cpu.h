b4af32344a6a99c775a6d9829c063f5ffb383e5d
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-11-27 15:25:25 +0100

    better sketchybar

--- .config/sketchybar/helper/cpu.h
+++ .config/sketchybar/helper/cpu.h
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <mach/mach.h>
 #include <stdbool.h>
 #include <time.h>
 
-static const char TOPPROC[32] = { "/bin/ps -Aceo pid,pcpu,comm -r" }; 
-static const char FILTER_PATTERN[16] = { "com.apple." };
+#define MAX_TOPPROC_LEN 28
+
+static const char TOPPROC[] = { "/bin/ps -Aceo pid,pcpu,comm -r" }; 
+static const char FILTER_PATTERN[] = { "com.apple." };
 
 struct cpu {
   host_t host;
@@ -68,7 +71,7 @@ static inline void cpu_update(struct cpu* cpu) {
     fgets(line, sizeof(line), file);
 
     char* start = strstr(line, FILTER_PATTERN);
-    char topproc[64];
+    char topproc[MAX_TOPPROC_LEN + 4];
     uint32_t caret = 0;
     for (int i = 0; i < sizeof(line); i++) {
       if (start && i == start - line) {
@@ -76,32 +79,39 @@ static inline void cpu_update(struct cpu* cpu) {
         continue;
       }
 
+      if (caret >= MAX_TOPPROC_LEN && caret <= MAX_TOPPROC_LEN + 2) {
+        topproc[caret++] = '.';
+        continue;
+      }
+      if (caret > MAX_TOPPROC_LEN + 2) break;
       topproc[caret++] = line[i];
       if (line[i] == '\0') break;
     }
+
+    topproc[MAX_TOPPROC_LEN + 3] = '\0';
 
     pclose(file);
 
     char color[16];
     if (total_perc >= .7) {
-      snprintf(color, 16, "0xffed8796");
+      snprintf(color, 16, "%s", getenv("RED"));
     } else if (total_perc >= .3) {
-      snprintf(color, 16, "0xfff5a97f");
+      snprintf(color, 16, "%s", getenv("ORANGE"));
     } else if (total_perc >= .1) {
-      snprintf(color, 16, "0xffeed49f");
+      snprintf(color, 16, "%s", getenv("YELLOW"));
     } else {
-      snprintf(color, 16, "0xffcad3f5");
+      snprintf(color, 16, "%s", getenv("LABEL_COLOR"));
     }
 
     snprintf(cpu->command, 256, "--push cpu.sys %.2f "
                                 "--push cpu.user %.2f "
-                                "--set cpu.percent label=%.0f%% label.color=%s "
-                                "--set cpu.top label=\"%s\"",
+                                "--set cpu.top label='%s' "
+                                "--set cpu.percent label=%.0f%% label.color=%s ",
                                 sys_perc,
                                 user_perc,
+                                topproc,
                                 total_perc*100.,
-                                color,
-                                topproc                                         );
+                                color          );
   }
   else {
     snprintf(cpu->command, 256, "");
--- .config/sketchybar/items/calendar.sh
+++ .config/sketchybar/items/calendar.sh
@@ -1,15 +1,16 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-sketchybar --add item     calendar right               \
-           --set calendar icon=cal                     \
-                          icon.color=$BLACK            \
-                          icon.font="$FONT:Black:12.0" \
-                          icon.padding_left=5          \
-                          icon.padding_right=5         \
-                          icon.drawing=off             \
-                          label.color=$BLACK           \
-                          label.padding_left=5         \
-                          label.padding_right=5        \
-                          background.color=0xffb8c0e0  \
-                          background.height=26         \
-                          background.corner_radius=11
+calendar=(
+  icon=􀐫
+  icon.font="$FONT:Black:12.0"
+  icon.padding_right=0
+  label.align=right
+  padding_left=15
+  update_freq=30
+  script="$PLUGIN_DIR/calendar.sh"
+  click_script="$PLUGIN_DIR/zen.sh"
+)
+
+sketchybar --add item calendar right       \
+           --set calendar "${calendar[@]}" \
+           --subscribe calendar system_woke
