36a3227c256e6d6f4616f98241f34cbeb6652332
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-11-30 07:03:38 +0100

    better sketchypar

--- .config/sketchybar/plugins/zen.sh
+++ .config/sketchybar/plugins/zen.sh
@@ -1,24 +1,43 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ $1 = "on" ]; then
-  sketchybar --set github.bell drawing=off \
+zen_on() {
+  sketchybar --set wifi drawing=off \
              --set apple.logo drawing=off \
              --set '/cpu.*/' drawing=off \
              --set calendar icon.drawing=off \
-             --set system.yabai drawing=off \
              --set separator drawing=off \
              --set front_app drawing=off \
+             --set volume_icon drawing=off \
+             --set spotify.anchor drawing=off \
              --set spotify.play updates=off \
-             --set spotify.title drawing=off \
-             --set spotify.artist drawing=off
-else
-  sketchybar --set github.bell drawing=on \
+             --set brew drawing=off \
+             --set volume drawing=off \
+             --set github.bell drawing=off
+}
+
+zen_off() {
+  sketchybar --set wifi drawing=on \
              --set apple.logo drawing=on \
              --set '/cpu.*/' drawing=on \
              --set calendar icon.drawing=on \
              --set separator drawing=on \
              --set front_app drawing=on \
-             --set system.yabai drawing=on \
-             --set spotify.play updates=on
+             --set volume_icon drawing=on \
+             --set spotify.play updates=on \
+             --set brew drawing=on \
+             --set volume drawing=on \
+             --set github.bell drawing=on
+}
+
+if [ "$1" = "on" ]; then
+  zen_on
+elif [ "$1" = "off" ]; then
+  zen_off
+else
+  if [ "$(sketchybar --query apple.logo | jq -r ".geometry.drawing")" = "on" ]; then
+    zen_on
+  else
+    zen_off
+  fi
 fi
 
