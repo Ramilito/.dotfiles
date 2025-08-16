efb093d0f82307e6935cd609f2e1b6d4f052e5e0
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-08-16 11:47:24 +0200

    fix obsidian config

--- .config/nvim/lua/user/plugins/obsidian.lua
+++ .config/nvim/lua/user/plugins/obsidian.lua
@@ -1,24 +1,10 @@
 return {
   "obsidian-nvim/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
+  version = "*",
   lazy = true,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
-  dependencies = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
-
-    -- see above for full list of optional dependencies ☝️
-  },
   ---@module 'obsidian'
-  ---@type obsidian.config.ClientOpts
+  ---@type obsidian.config
   opts = {
     workspaces = {
       {
@@ -30,90 +16,5 @@ return {
         path = "~/workspace/podme/notes",
       },
     },
-
-    -- see below for full list of options 👇
   },
 }
-
-
--- return {
---   "epwalsh/obsidian.nvim",
---   version = "*", -- recommended, use latest release instead of latest commit
---   lazy = true,
---   ft = "markdown",
---   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
---   -- event = {
---   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
---   --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
---   --   -- refer to `:h file-pattern` for more examples
---   --   "BufReadPre path/to/my-vault/*.md",
---   --   "BufNewFile path/to/my-vault/*.md",
---   -- },
---   dependencies = {
---     -- Required.
---     "nvim-lua/plenary.nvim",
---     -- "saghen/blink.cmp",
---     -- "hrsh7th/nvim-cmp",
---     -- "nvim-telescope/telescope.nvim",
---     "ibhagwan/fzf-lua",
---     "nvim-treesitter/nvim-treesitter",
---   },
---   opts = {
---     daily_notes = {
---       -- Optional, if you keep daily notes in a separate directory.
---       folder = "notes/dailies",
---       -- Optional, if you want to change the date format for the ID of daily notes.
---       date_format = "%Y-%m-%d",
---       -- Optional, if you want to change the date format of the default alias of daily notes.
---       alias_format = "%B %-d, %Y",
---       -- Optional, default tags to add to each new daily note created.
---       default_tags = { "daily-notes" },
---       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
---       template = nil,
---     },
---
---     workspaces = {
---       {
---         name = "personal",
---         path = "~/workspace/mine",
---       },
---       {
---         name = "podme",
---         path = "~/workspace/podme/notes",
---       },
---     },
---
---     -- see below for full list of options 👇
---     completion = {
---       nvim_cmp = false,
---       min_chars = 2,
---     },
---     sources = {
---       completion = {
---         -- remember to enable your providers here
---         enabled_providers = { "lsp", "path", "snippets", "buffer" },
---       },
---     },
---     ui = {
---       enable = false,
---     },
---     picker = {
---       -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
---       name = "fzf-lua",
---       -- Optional, configure key mappings for the picker. These are the defaults.
---       -- Not all pickers support all mappings.
---       note_mappings = {
---         -- Create a new note from your query.
---         new = "<C-x>",
---         -- Insert a link to the selected note.
---         insert_link = "<C-l>",
---       },
---       tag_mappings = {
---         -- Add tag(s) to current note.
---         tag_note = "<C-x>",
---         -- Insert a tag at the current location.
---         insert_tag = "<C-l>",
---       },
---     },
---   },
--- }
