b595ed315e8a04d11a60b1ccffa4024e25f121df
Rami Daghlawi <rami.daghlawi@gmail.com>
2026-04-04 18:32:44 +0200

    add map

--- .config/nvim/lua/user/keymappings.lua
+++ .config/nvim/lua/user/keymappings.lua
@@ -98,5 +98,8 @@ Map("n", "gl", vim.diagnostic.open_float, { desc = "Open [d]iagnostics (float)" 
 Map("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
 Map("t", "<Esc>", [[<C-\><C-n>]], { remap = false, desc = "Exit terminal mode" })
 
+Map("n", "<leader>ka", "<cmd>Kubectl view alias<cr>", { desc = "Kubectl alias" })
+
+
 -- Mini
 Map("n", "<leader>e", '<cmd>lua require("mini.files").open(vim.api.nvim_buf_get_name(0), true)<cr>', { desc = "Open mini.files" })
